f71c1a8f82539672ef807836021045666f321672
arclayto <claytoraustin@gmail.com>
2017-02-14 19:36:06 -0600

    Add files via upload
    Here's my SQL schema practice, still a bit confused on using REFERENCES but I think it would be easier to articulate that question tomorrow after class.

---
+++ classMaterials/schemaPractice/austin_seeder.sql
@@ -0,0 +1,80 @@
+/**
+	Practice schema using SQL written by Austin
+ */
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id             INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
+	username       VARCHAR(50)  NOT NULL ,
+	password       VARCHAR(255) NOT NULL ,
+	street_adress  VARCHAR(255),
+	city           VARCHAR(50),
+	state_id       INT REFERENCES state (id),
+	zip            INT(7),
+	email          VARCHAR(50),
+	post_office    INT(10),
+	role_id        INT(10) REFERENCES roles (id) ,
+	modified_by    INT(10) REFERENCES users (id),
+	created_at     TIMESTAMP DEFAULT current_timestamp ,
+	updated_at     TIMESTAMP DEFAULT current_timestamp ,
+	last_login     TIMESTAMP DEFAULT current_timestamp
+);
+
+DROP TABLE IF EXISTS state;
+CREATE TABLE state (
+	id         INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
+	state       VARCHAR(12) NOT NULL
+	-- 12 because RI has the longest name of any state with 12 characters
+	-- we could just as easily make this 2 characters using abbreviation
+);
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
+	id         INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
+	type       VARCHAR(50) NOT NULL ,
+	created_at TIMESTAMP                    DEFAULT CURRENT_TIMESTAMP ,
+	updated_at TIMESTAMP                    DEFAULT current_timestamp
+);
+
+DROP TABLE IF EXISTS packages;
+CREATE TABLE packages (
+	id             INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
+	user_id        INT(10) UNSIGNED NOT NULL REFERENCES users (id) ,
+	return_address VARCHAR(255),
+	destination    VARCHAR(255)     NOT NULL ,
+	-- I really think we should make address it's own table as adrian suggested
+	-- it would be especially useful here so we don't have 6 individual attributes
+	-- having to do with addresses
+	status         INT REFERENCES status (id) ,
+	contents       VARCHAR(255) ,
+	weight         DOUBLE ,
+	priority       BOOL ,
+	transaction_id INT(10) REFERENCES transactions (id) ,
+	created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+	updated_at     TIMESTAMP DEFAULT current_timestamp
+);
+
+DROP TABLE IF EXISTS status;
+CREATE TABLE status (
+	id    INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
+	type  VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS transactions;
+CREATE TABLE transactions (
+	id             INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
+	user_id        INT(10) REFERENCES users (id) ,
+	employee_id    INT(10) REFERENCES users (id) ,
+	post_office_id INT(10) REFERENCES post_offices (id) ,
+	cost           DOUBLE ,
+	package_id     INT(10) REFERENCES packages (id) ,
+	created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS post_offices;
+CREATE TABLE post_offices (
+	id       INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
+	name     VARCHAR(255) NOT NULL ,
+	city     VARCHAR(50),
+	state_id INT REFERENCES state (id),
+	zip      INT(7)
+);
