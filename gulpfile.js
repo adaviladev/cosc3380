ed2bee4b72ff2b7bf75a8e571d7c4b39160930fe
adrian <adavila.dev@gmail.com>
2017-03-15 19:48:16 -0500

    Updated AuthController@signIn() logic to comply with new QueryBuilder API. Added PackageController@postOfficeInventory() for displaying all packages at a given post office. Updated PackageController@show() to comply with new QueryBuilder API and added database calls for addresses table Updated PackageController@packageDetail() to comply with new QueryBuilder API. Added PagesController@error() to give default view for pages not in Router::routes[]. Updated postOfficeId values in /core/database/sql/packagesSeeder.sql. Added /core/database/sql/postOfficeSeeder.sql Added same password to all users in /core/database/sql/usersSeeder.sql. Switched boolean for QueryBuilder::findAll() for isSingle to false. Moved boolean conjunction to append new conditions in QueryBuilder::where(). Added dd() to die and dump any passed values for easier debugging. Added future redirect to 404 pages for routes that don't exist. Updated CSS for forms Started adding JavaScript logic for material design forms. Removed temporary if(true){} check. Added /views/error/404.view.php to display generic error. Wrapped /views/packages/packageDetail.view.php markup in if to check if a package exists. Added new navigation items and classes for CSS updates Added new routes for /dashboard/packages and /404.

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require( 'gulp' );
+
+var jshint = require( 'gulp-jshint' );
+var sass = require( 'gulp-sass' );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
+var rename = require( 'gulp-rename' );
+
+gulp.task( 'lint', function() {
+	return gulp.src( 'views/assets/js/*.js' )
+		.pipe( jshint() )
+		.pipe( jshint.reporter( 'default' ) );
+} );
+
+gulp.task( 'css', function() {
+	return gulp.src( 'views/assets/css/*.css' )
+		.pipe( sass() )
+		.pipe( gulp.dest( 'dist/css' ) );
+} );
+
+gulp.task( 'scripts', function() {
+	return gulp.src( 'views/assets/js/*.js' )
+		.pipe( concat( 'all.js' ) )
+		.pipe( gulp.dest( 'dist' ) )
+		.pipe( rename( 'all.min.js' ) )
+		.pipe( uglify() )
+		.pipe( gulp.dest( 'dist/js' ) );
+} );
+
+gulp.task('watch', function() {
+	gulp.watch('js/*.js', ['lint', 'scripts']);
+	gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
