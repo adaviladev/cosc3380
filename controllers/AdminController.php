34aca452a36d3b779a87fd40e562bf31b125ef13
Krishan Desai <krishandesai507@gmail.com>
2017-03-19 14:56:32 -0500

    Added basic functions to show all tuples of packages, transactions, postOffices

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -10,6 +10,20 @@
 
     class AdminController {
         public function packages() {
+            $packages = Package::selectAll();
 
+            return view( 'index' , compact( 'packages' ) );
+        }
+
+        public function transactions() {
+            $transactions = Transaction::selectAll();
+
+            return view( 'index' , compact( 'transactions' ) );
+        }
+
+        public function postOffices() {
+            $postOffices = PostOffice::selectAll();
+
+            return view( 'index' , compact( 'postOffices' ) );
         }
 }
