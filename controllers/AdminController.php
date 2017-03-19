64fa67c880ca60105afcaec1954f47639301059c
Krishan Desai <krishandesai507@gmail.com>
2017-03-19 14:42:47 -0500

    Created admin controller

---
+++ controllers/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace App\Controllers;
+
+    use Transaction;
+    use PostOffice;
+    use App\Core\App;
+    use Role;
+    use User;
+
+    class AdminController {
+        public function packages() {
+
+        }
+}
