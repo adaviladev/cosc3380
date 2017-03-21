6c3eac8b5ce524d7c2d94c94f2dbb6ad0a24dca6
Krishan Desai <krishandesai507@gmail.com>
2017-03-20 19:47:47 -0500

    Methods now return to the correct view

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -2,28 +2,32 @@
 
     namespace App\Controllers;
 
+    use Package;
     use Transaction;
     use PostOffice;
-    use App\Core\App;
-    use Role;
-    use User;
 
     class AdminController {
         public function packages() {
             $packages = Package::selectAll();
 
-            return view( 'index' , compact( 'packages' ) );
+            return view( 'admin/adminPackages' , compact( 'packages' ) );
         }
 
         public function transactions() {
             $transactions = Transaction::selectAll();
 
-            return view( 'index' , compact( 'transactions' ) );
+            return view( 'admin/adminTransactions' , compact( 'transactions' ) );
         }
 
         public function postOffices() {
             $postOffices = PostOffice::selectAll();
 
-            return view( 'index' , compact( 'postOffices' ) );
+            return view( 'admin/adminPostOffices' , compact( 'postOffices' ) );
+        }
+
+        public function admin() {
+            $packages = Package::selectAll();
+
+            return view( 'admin/admin' , compact( 'packages' ) );
         }
 }
