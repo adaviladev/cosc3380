727b673367717d1b45bcdc76d20d4623039a1d26
Krishan Desai <krishandesai507@gmail.com>
2017-03-22 18:07:34 -0500

    Functional view for viewing all packages in the database

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -5,10 +5,47 @@
     use Package;
     use Transaction;
     use PostOffice;
+    use User;
+    use Address;
+    use State;
+    use PackageStatus;
 
     class AdminController {
         public function packages() {
             $packages = Package::selectAll();
+
+            foreach( $packages as $package ) {
+                $package->user          = User::find()
+                    ->where( [ 'id' ] ,
+                        [ '=' ] ,
+                        [ $package->userId ] )
+                    ->get();
+                $package->destination   = Address::find()
+                    ->where( [ 'id' ] ,
+                        [ '=' ] ,
+                        [ $package->destinationId ] )
+                    ->get();
+                $package->destination->state = State::find()
+                    ->where( ['id']  ,
+                        [ '=' ] ,
+                        [ $package->destination->stateId] )
+                    ->get();
+                $package->returnAddress = Address::find()
+                    ->where( [ 'id' ] ,
+                        [ '=' ] ,
+                        [ $package->returnAddressId ] )
+                    ->get();
+                $package->returnAddress->state = State::find()
+                    ->where( ['id']  ,
+                        [ '=' ] ,
+                        [ $package->returnAddress->stateId] )
+                    ->get();
+                $package->status = PackageStatus::find()
+                    ->where( ['id']  ,
+                        [ '=' ] ,
+                        [ $package->packageStatus] )
+                    ->get();
+            }
 
             return view( 'admin/adminPackages' , compact( 'packages' ) );
         }
