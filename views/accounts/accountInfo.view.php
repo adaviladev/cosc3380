ce595d7377d3151aeb1ad7752c7e521ec4f15d4b
austinc <claytoraustin@gmail.com>
2017-04-01 11:20:07 -0500

    Added preliminary account Info page

--- views/accounts/accountInfo.view.php
+++ views/accounts/accountInfo.view.php
@@ -4,17 +4,65 @@
 
 <div class="row">
 	<div class="container">
-		<h1>User Info</h1>
-	</div>
-</div>
-
-<?php if( ! empty( $user ) ) { ?>
-	<div class="row">
-		<div class="container">
-			<h1>test</h1>
+		<div class="form-wrapper">
+			<h2>Sign up!</h2>
+			<form action="/register" method="post">
+				<div class="field-container clearfix">
+					<label for="firstName">First Name</label>
+					<input type="text" name="firstName" class="" value="">
+				</div>
+				<!-- /.field-container -->
+				<div class="field-container clearfix">
+					<label for="lastName">Last name</label>
+					<input type="text" name="lastName" class="" value="">
+				</div>
+				<!-- /.field-container -->
+				<div class="field-container clearfix">
+					<label for="password">Password</label>
+					<input type="password" name="password" value="">
+				</div>
+				<!-- /.field-container -->
+				<div class="field-container validate clearfix">
+					<label for="email" class="email">Email</label>
+					<input type="text" name="email" value="" class="<?= $emailClass; ?>">
+				</div>
+				<!-- /.field-container -->
+				<div class="field-container clearfix">
+					<label for="address">Address</label>
+					<input type="text" name="address" value="">
+				</div>
+				<!-- /.field-container -->
+				<div class="field-container clearfix">
+					<label for="city">City</label>
+					<input type="text" name="city" value="">
+				</div>
+				<!-- /.field-container -->
+				<div class="field-container clearfix">
+					<label for="StateSelector">State</label>
+					<?php if( ! empty( $states ) ) { ?>
+						<select name="stateId" id="StateSelector">
+							<option disabled selected value=""></option>
+							<?php foreach( $states as $state ) { ?>
+								<option value="<?= $state->id ?>"><?= $state->state ?></option>
+							<?php } ?>
+						</select>
+					<?php } ?>
+				</div>
+				<!-- /.field-container -->
+				<!-- /#StateSelector -->
+				<div class="field-container clearfix">
+					<label for="zipCode">Zip Code</label>
+					<input type="text" name="zipCode" value="">
+				</div>
+				<!-- /.field-container -->
+				<button type="submit">Submit</button>
+			</form>
 		</div>
+		<!-- /.form-wrapper -->
 	</div>
-<?php } ?>
+	<!-- /.container -->
+</div>
+<!-- /.row -->
 
 <?php
 	getFooter();
