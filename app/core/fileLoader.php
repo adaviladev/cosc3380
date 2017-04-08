2fa80bc72944b23688b8abf2fbb6489b2605e8b1
adrian <adavila.dev@gmail.com>
2017-04-08 00:39:14 -0500

    Refactored code structure for better security while on the live server.

---
+++ app/core/fileLoader.php
@@ -0,0 +1,28 @@
+<?php
+
+	require __DIR__ . '/Model.php';
+
+	/**
+	 * These two might need to be refactored elsewhere.
+	 * Still haven't decided on how to incorporate
+	 * the Auth class for logging users in.
+	 */
+	foreach( glob( __DIR__ . '/classes/*.php' ) as $file ) {
+		require_once $file;
+	}
+
+	/**
+	 * Keep growing this list as the application grows.
+	 */
+	require __DIR__ . '/App.php';
+	require __DIR__ . '/Router.php';
+	require __DIR__ . '/Request.php';
+	require __DIR__ . '/database/Connection.php';
+	require __DIR__ . '/database/QueryBuilder.php';
+
+	/**
+	 * Auto-require all controllers in controllers directory
+	 */
+	foreach( glob( __DIR__ . '/../controllers/*.php' ) as $file ) {
+		require_once $file;
+	}
