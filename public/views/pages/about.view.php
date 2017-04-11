452f185c77b168d81e1e99a631b9e5831d7031c0
DahburaV <herrera.vik@gmail.com>
2017-04-11 08:47:20 -0500

    Styling idea for About Us page

--- public/views/pages/about.view.php
+++ public/views/pages/about.view.php
@@ -11,44 +11,22 @@ or just prostoffice
 
 <?php getHeader(); ?>
 <center>
-	<div class = "row">
-		<div class = "container">
-			<div style = 'font:25px Arial;color:#888888'>
-				About Us
-			</div>
-			<!-- end style/color -->
-		</div>
-		<!-- end container -->
+<div class="banner" style="margin: -35px">
+	<div class="banner-content" style="font:16px Arial; color:#888888">
+		<h1>About Us</h1>
+		&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Prostoffice.pro is a
+		postal service founded on the ideals of speed, efficiency,
+		ease, and most importantly, reliability. Every day we strive
+		to be the best postal service available. Why? Because we
+		believe customer satisfaction and happiness are the two most
+		important aspects of running a successful and useful service
+		that our customers will yearn to return to.
+		<br />
+		<br />
+		Customer satisfaction is a priority to us here at Prostoffice.pro,
+		so if you have any issues please <a href="/contact">contact us</a>.
 	</div>
-	<!-- end row -->
-</center>
-<!-- title -->
-
-<center>
-	<div class = "row">
-		<div class = "container">
-			<div class= "paragraph-breaking">
-				<div style = 'font:17px Arial; color:#888888'>
-					<br />
-					&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Prostoffice.pro is a
-					postal service founded on the ideals of speed, efficiency,
-					ease, and most importantly, reliability. Every day we strive
-					to be the best postal service available. Why? Because we
-					believe customer satisfaction and happiness are the two most
-					important aspects of running a successful and useful service
-					that our customers will yearn to return to.
-					<br />
-					<br />
-					Customer satisfaction is a priority to us here at Prostoffice.pro,
-					so if you have any issues please <a href="/contact">contact us</a>.
-				</div>
-					<!-- end style/color-->
-			</div>
-				<!-- end paragraph word breaking -->
-		</div>
-			<!-- end container-->
-	</div>
-<!-- end row -->
+</div>
 </center>
 
 <?php getFooter(); ?>
