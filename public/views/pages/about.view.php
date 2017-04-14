a5b6f2aa4c634310fff3d525fbe70d4825f3ac72
adrian <adavila.dev@gmail.com>
2017-04-14 13:17:50 -0500

    Fixed About page styling.

--- public/views/pages/about.view.php
+++ public/views/pages/about.view.php
@@ -1,32 +1,20 @@
-
-<!--
---About Us page--
-points of contention:
-- whether or not we should be referred to as prostoffice.pro
-or just prostoffice
-
-- where to put a link to this page (I was thinking in the footer)
--
--->
-
 <?php getHeader(); ?>
-<center>
-<div class="banner" style="margin: -35px">
-	<div class="banner-content" style="font:16px Arial; color:#888888">
-		<h1>About Us</h1>
-		&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Prostoffice.pro is a
-		postal service founded on the ideals of speed, efficiency,
-		ease, and most importantly, reliability. Every day we strive
-		to be the best postal service available. Why? Because we
-		believe customer satisfaction and happiness are the two most
-		important aspects of running a successful and useful service
-		that our customers will yearn to return to.
-		<br />
-		<br />
-		Customer satisfaction is a priority to us here at Prostoffice.pro,
-		so if you have any issues please <a href="/contact">contact us</a>.
+
+	<div class="banner">
+		<div class="banner-content card">
+			<h1>About Us</h1>
+			Prostoffice.pro is a
+			postal service founded on the ideals of speed, efficiency,
+			ease, and most importantly, reliability. Every day we strive
+			to be the best postal service available. Why? Because we
+			believe customer satisfaction and happiness are the two most
+			important aspects of running a successful and useful service
+			that our customers will yearn to return to.
+			<br/>
+			<br/>
+			Customer satisfaction is a priority to us here at Prostoffice.pro,
+			so if you have any issues please <a href="/contact">contact us</a>.
+		</div>
 	</div>
-</div>
-</center>
 
 <?php getFooter(); ?>
