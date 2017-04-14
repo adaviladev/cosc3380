f80d1627f1a54c3bc16ce33a4c5fffc4a06e1d0f
adrian <adavila.dev@gmail.com>
2017-04-14 14:52:25 -0500

    Added comments and removed printed statements Added updates for all status types to keep the packages cycling between all states

--- app/core/database/cron/updatePackages.php
+++ app/core/database/cron/updatePackages.php
@@ -10,35 +10,71 @@
 
 	require __DIR__ . '/../../bootstrap.php';
 
-	$enRoute = Package::findAll()
-	              ->where( [ 'packageStatus' ] , [ '=' ] , [ 2 ] )
-	              ->get();
-
 	$processing = Package::findAll()
-	                 ->where( [ 'packageStatus' ] , [ '=' ] , [ 1 ] )
-	                 ->get();
+	                     ->where( [ 'packageStatus' ] , [ '=' ] , [ 1 ] )
+	                     ->get();
 
+	$enRoute = Package::findAll()
+	                  ->where( [ 'packageStatus' ] , [ '=' ] , [ 2 ] )
+	                  ->get();
+
+	$delivered = Package::findAll()
+	                     ->where( [ 'packageStatus' ] , [ '=' ] , [ 3 ] )
+	                     ->get();
+
+	$cancelledList = Package::findAll()
+	                        ->where( [ 'packageStatus' ] , [ '=' ] , [ 4 ] )
+	                        ->get();
+
+	/**
+	 * Increment packageStatus property of packages collected above
+	 */
 	$ctr = 0;
-	foreach( $enRoute as $package ) {
-		$rand = rand( 1 , 10 );
-		if( $rand <= 3 ) {
-			Package::update( [
-				   'packageStatus' => $package->packageStatus + 1
-			   ] )
-			   ->where( [ 'id' ] , [ '=' ] , [ $package->id ] )
-			   ->get();
-			$ctr++;
-		}
-	}
 	foreach( $processing as $package ) {
 		$rand = rand( 1 , 10 );
 		if( $rand <= 3 ) {
 			// var_dump( $package );
 			Package::update( [
-				   'packageStatus' => $package->packageStatus + 1
-			   ] )
-			   ->where( [ 'id' ] , [ '=' ] , [ $package->id ] )
-			   ->get();
+				                 'packageStatus' => $package->packageStatus + 1
+			                 ] )
+			       ->where( [ 'id' ] , [ '=' ] , [ $package->id ] )
+			       ->get();
+			$ctr++;
+		}
+	}
+	$ctr = 0;
+	foreach( $enRoute as $package ) {
+		$rand = rand( 1 , 10 );
+		if( $rand <= 3 ) {
+			Package::update( [
+				                 'packageStatus' => $package->packageStatus + 1
+			                 ] )
+			       ->where( [ 'id' ] , [ '=' ] , [ $package->id ] )
+			       ->get();
+			$ctr++;
+		}
+	}
+	$ctr = 0;
+	foreach( $delivered as $package ) {
+		$rand = rand( 1 , 10 );
+		if( $rand <= 3 ) {
+			Package::update( [
+				                 'packageStatus' => $package->packageStatus + 1
+			                 ] )
+			       ->where( [ 'id' ] , [ '=' ] , [ $package->id ] )
+			       ->get();
+			$ctr++;
+		}
+	}
+	$ctr = 0;
+	foreach( $cancelledList as $package ) {
+		$rand = rand( 1 , 10 );
+		if( $rand <= 3 ) {
+			Package::update( [
+				                 'packageStatus' => 1
+			                 ] )
+			       ->where( [ 'id' ] , [ '=' ] , [ $package->id ] )
+			       ->get();
 			$ctr++;
 		}
 	}
