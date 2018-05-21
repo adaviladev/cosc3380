e47de9e760748b99de6870b81e07df0686044c4a
adrian <adavila.dev@gmail.com>
2018-05-20 19:11:43 -0500

    add 'app' id to body tag

--- public/views/partials/header.php
+++ public/views/partials/header.php
@@ -10,7 +10,7 @@
 		<link rel="stylesheet" href="/assets/bundle/css/all.min.css">
 	</head>
 
-	<body class="card">
+	<body id="app" class="card">
 		<?php getPartial( 'nav' ) ?>
 		<div id="ContentWrapper">
 			<div class="row nav-menu card clearfix">
